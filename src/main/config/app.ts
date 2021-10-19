2446d22058887b351894debdde44f8be7a66c662
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-10-19 17:20:01 -0300

    refactor: uncoupling swagger and add access and auth to docs

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -3,8 +3,10 @@ import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import setupEnv from './env'
 import setupPGNumberOverride from './pg-numeric-format'
+import setupSwagger from './config-swagger'
 
 const app = express()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
 setupEnv()
--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,11 +1,7 @@
 import { Express } from 'express'
 import { bodyParser, cors, contentType } from '../middlewares'
-import swaggerUI from 'swagger-ui-express'
-import swaggerDocs from './swagger.json'
-
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
   app.use(contentType)
-  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 }
