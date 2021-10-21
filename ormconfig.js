25471973dba09db741775bc92385545aa86d989e
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-10-21 09:52:49 -0300

    chore: config of production to heroku deploy

--- ormconfig.js
+++ ormconfig.js
@@ -26,11 +26,7 @@ const prodConfig = [
   {
     name: 'default',
     type: 'postgres',
-    host: process.env.POSTGRES_DB_HOST,
-    port: process.env.POSTGRES_DB_PORT,
-    username: process.env.POSTGRES_DB_USERNAME,
-    password: process.env.POSTGRES_DB_PASSWORD,
-    database: process.env.POSTGRES_DB_DATABASE,
+    url: process.env.DATABASE_URL,
     synchronize: false,
     migrationsRun: false,
     entities: [
